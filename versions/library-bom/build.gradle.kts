234710a51e15bd9f9a810b830749e18ea631c05f
sander <spostma@sphereon.com>
2026-02-05 17:58:20 +0100

    chore: VDX-21 additions

--- versions/library-bom/build.gradle.kts
+++ versions/library-bom/build.gradle.kts
@@ -153,11 +153,29 @@ dependencies {
 
         // Testing - Testcontainers
         api("org.testcontainers:testcontainers:1.21.0")
+        api("org.testcontainers:postgresql:1.21.0")
+        api("org.testcontainers:mysql:1.21.0")
         api("org.testcontainers:junit-jupiter:1.21.0")
-        api("com.github.docker-java:docker-java-core:3.4.2")
-        api("com.github.docker-java:docker-java-transport-httpclient5:3.4.2")
-        api("com.github.docker-java:docker-java-api:3.4.2")
 
+        // Keycloak SPI
+        api("org.keycloak:keycloak-core:26.0.0")
+        api("org.keycloak:keycloak-server-spi:26.0.0")
+        api("org.keycloak:keycloak-server-spi-private:26.0.0")
+        api("org.keycloak:keycloak-services:26.0.0")
+        api("org.keycloak:keycloak-admin-client:26.0.0")
+
+        // JBoss Logging (provided by Keycloak at runtime)
+        api("org.jboss.logging:jboss-logging:3.6.1.Final")
+
+        // Testing
+        api("io.mockk:mockk:1.13.14")
+        api("org.junit.jupiter:junit-jupiter-engine:5.11.0")
+        api("org.slf4j:slf4j-simple:2.0.16")
+
+        // Docker-java (for Testcontainers)
+        api("com.github.docker-java:docker-java-core:3.4.1")
+        api("com.github.docker-java:docker-java-transport-httpclient5:3.4.1")
+        api("com.github.docker-java:docker-java-api:3.4.1")
 
         // Spring
         api("org.springframework.boot:spring-boot-starter-web:3.5.8")
